328756e341b4aa3cbfec65a948ee23bd8f45ee5c
peter-vdd <134661402+peter-vdd@users.noreply.github.com>
2023-05-26 17:35:33 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,65 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/widgets/index.dart'; // Imports other custom widgets
+import '/custom_code/actions/index.dart'; // Imports custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class PdfView extends StatefulWidget {
+  const PdfView({
+    Key? key,
+    this.width,
+    this.height,
+    this.docUrl,
+    this.pageNr,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+  final String? docUrl;
+  final int? pageNr;
+
+  @override
+  _PdfViewState createState() => _PdfViewState();
+}
+
+class _PdfViewState extends State<PdfView> {
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Syncfusion Flutter PDF Viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.bookmark,
+              color: Colors.white,
+              semanticLabel: 'Bookmark',
+            ),
+            onPressed: () {
+              _pdfViewerKey.currentState?.openBookmarkView();
+            },
+          ),
+        ],
+      ),
+      body: SfPdfViewer.network(
+        'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+        key: _pdfViewerKey,
+      ),
+    );
+  }
+}
